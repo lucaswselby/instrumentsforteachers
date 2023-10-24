3582768e0260314aa03033a89593df3e871e5e59
lucaswselby <41557042+lucaswselby@users.noreply.github.com>
2023-10-23 23:30:19 -0700

    Instruments filter

--- main.js
+++ main.js
@@ -39,9 +39,53 @@ class Program {
     }
 }
 
+// initializes school programs
 const rhodesO = new Program("AZ", "O", "Rhodes Junior High School", "4804722300", "1860 S Longmore, Mesa, AZ 85202", "Sarina Mountcastle", "samountcastle@mpsaz.org", ["3/4 Violins", "4/4 Violins"], "October 18, 2023");
 const rhodesB = new Program("AZ", "B", "Rhodes Junior High School", "4804722300", "1860 S Longmore, Mesa, AZ 85202", "Calle Thuneman", "cfthuneman@mpsaz.org", ["Trumpets"], "October 18, 2023");
 const programs = [rhodesO, rhodesB];
+
+// initializes instrument type arrays
+const bandInstruments = ["Piccolos", "Flutes", "Oboes", "Clarinets", "Bass Clarinets", "Alto Saxophones", "Tenor Saxophones", "Trumpets", "Cornets", "French Horns", "Trombones", "Baritones", "Euphoniums", "Bell Kits", "Percussion Learning Kits", "Drum Kits"];
+const orchestraInstruments = ["1/4 Violins", "1/2 Violins", "3/4 Violins", "4/4 Violins", "12\" Violas", "13\" Violas", "14\" Violas", "15\" Violas", "15.5\" Violas", "16\" Violas", "1/4 Cellos", "1/2 Cellos", "3/4 Cellos", "4/4 Cellos", "1/4 String Basses", "1/2 String Basses", "3/4 String Basses"];
+
+// displays all programs of a specific type within a certain state
+const displayPrograms = () => {
+    document.getElementsByTagName("TBODY")[0].innerHTML = "";
+    programs.filter(program => {
+        let programHasInstrument = false;
+        if (program.state === document.getElementById("state").value && program.bo === document.getElementById("program").value) {
+            (document.getElementById("program").value === "B" ? bandInstruments : orchestraInstruments).filter(instrument => document.getElementById(instrument) && document.getElementById(instrument).checked).forEach(instrument => {
+                if (program.needs.includes(instrument)) {
+                    programHasInstrument = true;
+                }
+            });
+        }
+        return programHasInstrument;
+    }).forEach(program => {
+        document.getElementsByTagName("TBODY")[0].innerHTML += `<tr>
+            <td>${program.school}</td>
+            <td><a href="tel:+1${program.phone}">${formatPhoneNumber(program.phone)}</a></td>
+            <td><a href="https://maps.google.com/maps?q=${program.address}" target="_blank">${program.address}</a></td>
+            <td>${program.teacher}</td>
+            <td><a href="mailto:${program.email}" target="_blank">${program.email}</a></td>
+            <td>${program.needs.join(", ")}</td>
+        </tr>`;
+    });
+};
+
+// instrument filters change based on the program
+const fillInstrumentsFilter = () => {
+    document.getElementById("instruments").innerHTML = "<summary>Instruments</summary>";
+    (document.getElementById("program").value === "B" ? bandInstruments : orchestraInstruments).forEach(instrument => {
+        document.getElementById("instruments").innerHTML += `<div>
+            <input type="checkbox" name="${instrument}" id="${instrument}" checked>
+            <label for="${instrument}">${instrument}</label>
+        </div>`
+    });
+    (document.getElementById("program").value === "B" ? bandInstruments : orchestraInstruments).forEach(instrument => {
+        document.getElementById("instruments").onclick = displayPrograms;
+    });
+};
 
 // source: https://stackoverflow.com/questions/8358084/regular-expression-to-reformat-a-us-phone-number-in-javascript
 function formatPhoneNumber(phoneNumberString) {
@@ -53,22 +97,10 @@ function formatPhoneNumber(phoneNumberString) {
     return null;
 }
 
-const displayPrograms = () => {
-    document.getElementsByTagName("TBODY")[0].innerHTML = "";
-    programs.forEach(program => {
-        if (program.state === document.getElementById("state").value && program.bo === document.getElementById("program").value) {
-            document.getElementsByTagName("TBODY")[0].innerHTML += `<tr>
-                <td>${program.school}</td>
-                <td><a href="tel:+1${program.phone}">${formatPhoneNumber(program.phone)}</a></td>
-                <td><a href="https://maps.google.com/maps?q=${program.address}" target="_blank">${program.address}</a></td>
-                <td>${program.teacher}</td>
-                <td><a href="mailto:${program.email}">${program.email}</a></td>
-                <td>${program.needs.join(", ")}</td>
-            </tr>`;
-        }
-    });
-};
+fillInstrumentsFilter();
 displayPrograms();
-
 document.getElementById("state").onchange = displayPrograms;
-document.getElementById("program").onchange = displayPrograms;
+document.getElementById("program").onchange = () => {
+    fillInstrumentsFilter();
+    displayPrograms();
+}
