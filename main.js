89f4a350406183b504c3e98bd1795d97204ee367
lucaswselby <41557042+lucaswselby@users.noreply.github.com>
2023-11-02 00:11:47 -0700

    Teacher pictures

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ const displayPrograms = () => {
         filteredPrograms.forEach(program => {
             document.getElementById("featuredPrograms").innerHTML += `<div class="featuredProgram">
                 <div class="left">
-                    <img src="./images/teachers/default image.png" alt="${program.teacher}">
+                    <img src="./images/teachers/${`./images/teachers/${program.teacher}.png` ? program.teacher : "default image"}.png" alt="${program.teacher}">
                     <div class="teacher">${program.teacher}</div>
                     <div class"title">${program.bo === "B" ? "Band" : "Orchestra"} Teacher</div>
                     <div class="email"><a href="mailto:${program.email}" target="_blank">${program.email}</a></div>
